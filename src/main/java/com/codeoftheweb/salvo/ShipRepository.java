3bf427026200c1bf387a490e2844c01d007b8d5c
wekotsai <42733086+wekotsai@users.noreply.github.com>
2018-10-17 11:14:31 +0200

    new classes ship and shipRepository added

--- src/main/java/com/codeoftheweb/salvo/ShipRepository.java
+++ src/main/java/com/codeoftheweb/salvo/ShipRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
 @Component
-public interface PlayerRepository extends JpaRepository<Player, Long> {
+public interface ShipRepository extends JpaRepository<Ship, Long> {
 }
