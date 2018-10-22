f7daab6ad64f742241c9ceb63654698f0e5ebf17
wekotsai <42733086+wekotsai@users.noreply.github.com>
2018-10-22 12:19:15 +0200

    changed grid display

--- src/main/resources/static/web/game.js
+++ src/main/resources/static/web/game.js
@@ -1,26 +1,13 @@
-var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "A", "B", "C", "D", "E","F","G","H","I","J"],
-    tr;
+row = ["", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, ];
+col = ["","A", "B", "C", "D", "E","F","G","H","I","J"];
 
-array.forEach((v, i) => {
-    var td = document.createElement('td');
-
-    // if (!(i = isNaN))
-    if (!(i % 10)) {
-        tr = document.createElement('tr');
-        document.getElementById('table0').appendChild(tr);
+for (var r = 0; r < row.length; r++)
+{
+    var x = document.getElementById('myTable').insertRow(r);
+    for (var c = 0; c < col.length; c++)
+    {
+        var y =  x.insertCell(c);
+        y.innerHTML= row[r] + col[c];
     }
-    td.appendChild(document.createTextNode(v));
-    tr.appendChild(td);
-});
+}
 
-// function getColumnsHtml(row) {
-//     return row.elements.map(function(element) {
-//         return "<td>" + element.distance.text + "</td>";
-//     }).join("")
-// }
-
-// $.ajax({
-//     type: 'POST',
-//     url: 'http://localhost:8080/api/game_view/{nn}',
-//     data: 'url=' + window.location.toString()
-// });
