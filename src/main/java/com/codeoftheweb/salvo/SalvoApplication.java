43974d76e0c0c7ecc8bb0c0622a771a54803c906
wekotsai <42733086+wekotsai@users.noreply.github.com>
2018-10-31 11:08:01 +0100

    Login and logout work perfectly

--- src/main/java/com/codeoftheweb/salvo/SalvoApplication.java
+++ src/main/java/com/codeoftheweb/salvo/SalvoApplication.java
@@ -14,7 +14,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.WebAttributes;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.util.Arrays;
 import java.util.List;
 
@@ -154,11 +159,16 @@ class WebSecurityConfiguration extends GlobalAuthenticationConfigurerAdapter {
 
     @Override
     public void init(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(inputName-> {
+        auth.userDetailsService(inputName -> {
+            System.out.println(inputName);
             Player player = playerRepository.findByEmail(inputName);
+            System.out.println(player);
             if (player != null) {
-                return new User(player.getEmail(), player.getPassword(),
-                        AuthorityUtils.createAuthorityList("USER"));
+                return User.withDefaultPasswordEncoder()
+                        .username(player.getEmail())
+                        .password(player.getPassword())
+                        .roles("USER")
+                        .build();
             } else {
                 throw new UsernameNotFoundException("Unknown user: " + inputName);
             }
@@ -173,14 +183,40 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/**").hasAuthority("USER")
-                .anyRequest().denyAll();
+//                .antMatchers("/**").permitAll();
+                .antMatchers("/web/games.html").permitAll()
+                .antMatchers("/web/games.js").permitAll()
+                .antMatchers("/web/style.css").permitAll()
+                .antMatchers("/api/games").permitAll()
+                .antMatchers("/**").hasAuthority("USER");
 
         http.formLogin()
-                .usernameParameter("email")
-                .passwordParameter("pwd")
-                .loginPage("/api/login.html");
+                .usernameParameter("userName")
+                .passwordParameter("password")
+                .loginPage("/api/login");
 
         http.logout().logoutUrl("/api/logout");
+
+        // turn off checking for CSRF tokens
+        http.csrf().disable();
+
+        // if user is not authenticated, just send an authentication failure response
+        http.exceptionHandling().authenticationEntryPoint((req, res, exc) -> res.sendError(HttpServletResponse.SC_UNAUTHORIZED));
+
+        // if login is successful, just clear the flags asking for authentication
+        http.formLogin().successHandler((req, res, auth) -> clearAuthenticationAttributes(req));
+
+        // if login fails, just send an authentication failure response
+        http.formLogin().failureHandler((req, res, exc) -> res.sendError(HttpServletResponse.SC_UNAUTHORIZED));
+
+        // if logout is successful, just send a success response
+        http.logout().logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler());
+    }
+
+    private void clearAuthenticationAttributes(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+        }
     }
 }
